6cbd740b3000ad8d3bb9a979769db04b66a4d6da
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-08-04 21:50:34 -0300

    html of landing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
+import Landing from './pages/Landing'
+
 import './assets/styles/global.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <Landing />
   );
 }
 
